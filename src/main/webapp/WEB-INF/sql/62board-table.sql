f6f2f32316a01426de1f90c3f4b814027aa3b226
sebaek <sebaek@gmail.com>
2022-11-18 09:38:40 +0900

    권한 추가

--- src/main/webapp/WEB-INF/sql/62board-table.sql
+++ src/main/webapp/WEB-INF/sql/62board-table.sql
@@ -211,7 +211,23 @@ CREATE TABLE Authority (
     PRIMARY KEY (memberId, auth)
 );
 
+INSERT INTO Authority (memberId, auth)
+VALUES ('admin', 'admin');
 
+SELECT * FROM Authority;
+
+-- 권한테이블, 멤버테이블 조인 조회
+	SELECT 
+		id,
+		nickName,
+		password,
+		email,
+		inserted,
+        a.auth
+	FROM
+		Member m LEFT JOIN Authority a ON m.id = a.memberId
+	WHERE
+		id = 'admin';
 
 
 
